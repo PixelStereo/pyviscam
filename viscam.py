0c51cc0c992363034678f847f74ea8157b36cb76
reno- <contact@rubiano.fr>
2015-11-23 16:45:08 +0100

    add a working exemple

---
+++ viscam.py
@@ -0,0 +1,56 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from time import sleep
+#from PyDeviceManager.devicemanager import Serial as serial
+from PyVisca.PyVisca import _cmd_adress_set , Visca , _if_clear
+from PyVisca.PyVisca import Serial as serial
+
+debug = True
+
+if debug : print '-----serial port initialisation-----'
+# create a serial object
+serial = serial()
+# get a list of serial ports available and select the last one
+port = serial.listports()[-1]
+if debug : print 'serial port opening : ' + port
+# open a connection on the serial object
+serial.open(portname=port)
+# broadcase a address set command
+if debug : print '-----pyvisca module initialisation-----'
+viscams = _cmd_adress_set(serial)
+if debug : print '-----clearing all buffers-----'
+_if_clear(serial)
+
+# this part is ugly but I don't know how to do. the following commented code does'nt work
+"""for v in viscams:
+	print v
+	v = Visca(serial)"""
+if len(viscams) == 1:
+	v1 = Visca(serial)
+if len(viscams) == 2:
+	v1 = Visca(serial)
+	v2 = Visca(serial)
+if len(viscams) == 3:
+	v1 = Visca(serial)
+	v2 = Visca(serial)
+	v3 = Visca(serial)
+if len(viscams) > 3:
+	print 'only 3 cameras are working for now'
+	sys.exit(1)
+
+print 'power off'
+v1.power(0)
+sleep(1)
+print 'power on'
+v1.power(1)
+sleep(10)
+print 'left'
+v1.left()
+sleep(4)
+print ('stop')
+v1.stop()
+sleep(0.2)
+print 'home'
+v1.home()
+sleep(10)
