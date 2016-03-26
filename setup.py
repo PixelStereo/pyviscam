39db8f8e3cf93bfdf23e2a3dfecc845b1626afad
Renaud <contact@rubiano.fr>
2016-03-26 19:35:03 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,11 +5,11 @@ from distutils.core import setup
 setup(
   name = 'pyviscam',
   packages = ['pyviscam'], 
-  version = '0.0.1',
+  version = '0.0.2',
   description = 'Control camera through Visca protocol',
   author = 'Pixel Stereo',
   url='https://github.com/PixelStereo/pyviscam', 
-  download_url = 'https://github.com/PixelStereo/pyviscam/tarball/0.0.1.zip', 
+  download_url = 'https://github.com/PixelStereo/pyviscam/tarball/0.0.2.zip', 
   classifiers = [
     'Development Status :: 3 - Alpha',
     'Intended Audience :: Developers',
